cfe02f37c18db3b3a50f11837aa90e3e9db48695
V1ctorIsrael <155853558+V1ctorIsrael@users.noreply.github.com>
2026-09-23 23:09:32 -0300

    Add files via upload

--- index.php
+++ index.php
@@ -4,18 +4,19 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Login</title>
+    <link rel="stylesheet" href="style.css">
 </head>
 <body>
     <form action="login.php" method="post">
-        <h1>Login</h1>
+        <h1 class= "titulo">Login</h1>
         <?php if(isset($_GET['error'])) { ?> 
             <p class="error"><?php echo $_GET['error']; ?> </p>
            <?php } ?>
-           <label> Nome de Usuário </label>
+           <label class="usuarioPedido"> Nome de Usuário </label>
            <input type="text" name="usuario" placeholder="Nome"><br>
-           <label> Senha </label> 
+           <label class="senhaPedido"> Senha </label> 
             <input type="password" name="senha"  placeholder="Senha"><br>
-            <button type= "submit">Entrar</button>
+            <button class= "botao" type= "submit">Entrar</button>
     </form>
 </body>
 </html>
